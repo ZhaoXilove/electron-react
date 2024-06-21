e1d46179489e19c57f32c858136b14f4f96fff8a
fjy <1787673323@qq.com>
2024-06-21 14:31:51 +0800

    feat: package update

--- src/pages/PageOne.tsx
+++ src/pages/PageOne.tsx
@@ -1,9 +1,37 @@
+import { css } from '@linaria/core'
+import { styled } from '@linaria/react'
 import { useBoolean } from 'ahooks'
+
+const Button = styled.button`
+  height: 2rem;
+  padding: 0 0.5rem;
+  border-radius: 0.25rem;
+  cursor: pointer;
+
+  border: 1px red solid;
+  color: #ffffff;
+  background-color: #222;
+  &:hover {
+    background-color: #333;
+  }
+  &:active {
+    background-color: green;
+  }
+`
+
+const title = css`
+  font-size: 24px;
+  font-weight: bold;
+`
+
 const PageOne = () => {
   const [state, { toggle, setTrue, setFalse }] = useBoolean(true)
+  const handleClick = () => {
+    console.log(1)
+  }
   return (
     <div>
-      <p>Effects：{JSON.stringify(state)}</p>
+      <p className={title}>Effects：{JSON.stringify(state)}</p>
       <p>
         <button type="button" onClick={toggle}>
           Toggle
@@ -14,6 +42,7 @@ const PageOne = () => {
         <button type="button" onClick={setTrue}>
           Set true
         </button>
+        <Button onClick={handleClick}>I am clickable! 🫧</Button>
       </p>
     </div>
   )
